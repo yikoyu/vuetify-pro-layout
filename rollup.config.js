8e8c3ae3e3b81c9bc14628a78c77712353dcad1b
滑稽 <2282373181@qq.com>
2021-01-02 11:00:02 +0800

    build: rollup添加图片处理

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from 'rollup-plugin-commonjs'
 import typescript from 'rollup-plugin-typescript'
 import resolve from 'rollup-plugin-node-resolve'
 import css from 'rollup-plugin-css-porter'
+import image from '@rollup/plugin-image'
 import minimist from 'minimist'
 
 import { terser } from 'rollup-plugin-terser'
@@ -27,6 +28,7 @@ const config = {
       compileTemplate: true
     }),
     css({ dest: 'dist/vuetify-pro-layout.css' }),
+    image(),
     resolve({
       jsnext: true,
       main: true
