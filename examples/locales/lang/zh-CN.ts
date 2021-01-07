5f7306bdb57eff7af6384f5b4b39ca461390fb58
滑稽 <2282373181@qq.com>
2021-01-07 15:48:23 +0800

    feat: examples中添加pro-layout语言包

--- examples/locales/lang/zh-CN.ts
+++ examples/locales/lang/zh-CN.ts
@@ -1,5 +1,6 @@
 import momentCN from 'moment/locale/zh-cn'
 import vuetifyCN from 'vuetify/es5/locale/zh-Hans'
+import { zhCN as ProLayoutZhCN } from 'vuetify-pro-layout'
 
 const components = {
   momentName: 'zh-cn',
@@ -8,36 +9,7 @@ const components = {
 }
 
 const locale: { [x: string]: string } = {
-  logout: '退出登录',
-  install: '安装',
-  refresh: '刷新',
-
-  // app.settings
-  'app.setting': '设置',
-  'app.translations': '翻译',
-  'app.setting.theme': '主题',
-  'app.setting.theme.light': '浅色主题',
-  'app.setting.theme.dark': '深色主题',
-  'app.setting.theme.system': '跟随系统',
-  'app.setting.navigationmode': '导航模式',
-  'app.setting.navigationmode.side': '侧边布局',
-  'app.setting.navigationmode.top': '顶部布局',
-  'app.setting.contentwidth': '内容宽度',
-  'app.setting.contentwidth.fluid': '流式',
-  'app.setting.contentwidth.fixed': '定宽',
-
-  'app.setting.multiTab': '标签',
-  'app.setting.multiTab.uni': '单标签',
-  'app.setting.multiTab.multi': '多标签',
-  'app.setting.multiTab.allClose': '关闭全部',
-  'app.setting.multiTab.closeCurrent': '关闭当前页',
-  'app.setting.multiTab.snackbar.closeend': '这是最后一个标签，无法被关闭',
-  'app.setting.multiTab.snackbar.close': '关闭',
-
-  'app.setting.direction': '布局',
-  'app.setting.direction.ltr': 'LTR',
-  'app.setting.direction.rtl': 'RTL',
-  'app.setting.primarycolor': '主题色'
+  ...ProLayoutZhCN
 }
 
 export default {
