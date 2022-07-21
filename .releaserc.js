ca60cf7b5e27cef854f784141a193474891ce148
yikoyu <2282373181@qq.com>
2022-07-21 09:33:28 +0800

    config(semantic-release): update release-notes-generator config

--- .releaserc.js
+++ .releaserc.js
@@ -4,30 +4,72 @@ module.exports = {
     [
       '@semantic-release/commit-analyzer',
       {
-        releaseRules: [{
-            type: "perf",
-            release: "patch"
-          },
-          {
-            type: "refactor",
-            release: "patch"
-          }
+        "releaseRules": [
+          { breaking: true,     release: "major" },
+          { revert: true,       release: "patch" },
+          { type: "feat",       release: "minor" },
+          { type: "fix",        release: "patch" },
+          { type: "perf",       release: "patch" },
+          { type: "refactor",   release: "patch" },
+          { type: "test",       release: "patch" },
+          { type: "revert",     release: "patch" }
         ]
       }
     ], // 解析 commit 信息，默认就是 Angular 规范
     [
       '@semantic-release/release-notes-generator',
       {
+        preset: "conventionalcommits",
         presetConfig: {
           types: [{
+              type: "feat",
+              section: "Features",
+              hidden: false
+            },
+            {
+              type: "fix",
+              section: "Bug Fixes",
+              hidden: false
+            },
+            {
               type: "perf",
               section: "Performance",
-              "hidden": false
+              hidden: false
             },
             {
               type: "refactor",
               section: "Refactor",
-              "hidden": false
+              hidden: false
+            },
+            {
+              type: "test",
+              section: "Tests",
+              hidden: false
+            },
+            {
+              type: "revert",
+              section: "Revert",
+              hidden: false
+            },
+            {
+              type: "docs",
+              section: "Docs",
+              hidden: true
+            },
+            {
+              type: "style",
+              section: "Styles",
+              hidden: true
+            },
+            {
+              type: "build",
+              section: "Build",
+              hidden: true
+            },
+            {
+              type: "ci",
+              section: "CI/CD",
+              hidden: true
             }
           ]
         }
@@ -39,7 +81,12 @@ module.exports = {
         changelogFile: 'CHANGELOG.md' // 把发布日志写入该文件
       }
     ],
-    '@semantic-release/npm',
+    [
+      '@semantic-release/npm',
+      // {
+      //   npmPublish: false
+      // }
+    ],
     '@semantic-release/github',
     [
       '@semantic-release/git',
